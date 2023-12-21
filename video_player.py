7d87f7aad1b9af89f15548052b085833d34bda56
GWStuart <89712016+GWStuart@users.noreply.github.com>
2023-12-21 23:16:49 +0100

    added sound!!

--- video_player.py
+++ video_player.py
@@ -1,16 +1,24 @@
 from time import sleep
 from os import system
 import cv2
+from ffpyplayer.player import MediaPlayer
 from utils import *
 
-vid = cv2.VideoCapture("ignored/video/bad_apple.mp4")
+path = "ignored/video/crab_rave.mp4"
+vid = cv2.VideoCapture(path)
+audio = MediaPlayer(path)
+
 fps = vid.get(cv2.CAP_PROP_FPS)
 
 try:
     while True:
         ret, frame = vid.read()
+        audio_frame, val = audio.get_frame()
         if not ret:
             break
+        if val != 'eof' and audio_frame is not None:
+            #audio (idk what this does)
+            img, t = audio_frame
 
         print_image(frame)
 
