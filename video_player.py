4a7461045f3bf84da42bf9c67d04068a7670fa10
GWStuart <89712016+GWStuart@users.noreply.github.com>
2023-12-21 20:33:23 +0100

    majour improvements to video player

--- video_player.py
+++ video_player.py
@@ -1,27 +1,31 @@
+from time import sleep
+from os import system, get_terminal_size
 import cv2
-import os
-import time	
 
-vid = cv2.VideoCapture("ignored/video/crab_rave.mp4")
+vid = cv2.VideoCapture("ignored/video/bad_apple.mp4")
 fps = vid.get(cv2.CAP_PROP_FPS)
 
-character = "$" # @#█
+character = "█" # @#█$
 
-def print_image(image):
-	image = cv2.resize(image, (os.get_terminal_size().columns, os.get_terminal_size().lines))
-	line = "".join(["".join([f"\033[38;2;{pixel[2]};{pixel[1]};{pixel[0]}m{character}" for pixel in row]) + "\n" for row in image])
-	print(line)
+def get_image(image):
+    image = cv2.resize(image, (get_terminal_size().columns, get_terminal_size().lines - 1))
+    print("".join(["".join([f"\033[38;2;{pixel[2]};{pixel[1]};{pixel[0]}m{character}" for pixel in row]) + "\n" for row in image]), end='')
 
-try: 
-	while True:
-		ret, frame = vid.read()
-		if not ret:
-			break
+try:
+    system("cls")
+    # print("")
+    while True:
+        ret, frame = vid.read()
+        if not ret:
+            break
 
-		print_image(frame)
+        frame = get_image(frame)
+        print("\033[H", end="")
+        # print(f"\033[{0};{0}H")
 
-		time.sleep(1/fps)
+        sleep(1/fps)
 
 finally:
-	vid.release() 
-	print("\033[0m")
+    vid.release() 
+    print(f"\033[{get_terminal_size().columns};{get_terminal_size().lines - 2}H")
+    print("\033[0m")
