27f6de14b68f97977795b68c2aa2269c3e642104
GWStuart <89712016+GWStuart@users.noreply.github.com>
2024-02-29 21:46:53 +1000

    added README + more on the help message

---
+++ README.md
@@ -0,0 +1,11 @@
+# An ASCII image viewer and video player
+This is a simple python program that can be used to render images with coloured ascii in the terminal.
+
+It support png and jpg images, mp4 videos, and urls to youtube videos that can be dynimically streamed.
+
+See the help page for more info, `python render.py -h`
+
+# Things still to be done
+- Actually implement the grayscale system
+- Check that everything is working properly (speicfically with video playing)
+- Tidy everything up a bit really
--- render.py
+++ render.py
@@ -4,6 +4,12 @@ from utils import *
 
 description = """
 A terminal video player written in python
+
+Usage:
+render "camera"  --> attempts to access your camera and shows you the output
+render image.png --> renders the image. Currently only supports png and jpg files
+render video.mp4 --> plays the mp4 video. Can be combined with the -m flag to play mute
+render hhtps://  --> renders a youtube video given its url
 """
 
 parser = ArgumentParser(prog="render", description=description, formatter_class=RawDescriptionHelpFormatter)
